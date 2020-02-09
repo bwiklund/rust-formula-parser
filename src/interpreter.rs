1bb23447e3d1a131fea9b76476560718d3061b7c
Max Seiden <max@sigmacomputing.com>
2020-02-09 14:42:55 -0800

    run `cargo fmt` fix `cargo clippy`

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,49 +1,49 @@
 use crate::ast::{Expr, Operator};
 
 pub fn eval(expr: &Expr) -> Result<f64, String> {
-  return match expr {
-    Expr::Call { args, target } => {
-      let mut arg_results = vec![];
-      for arg in args {
-        arg_results.push(eval(arg)?);
-      }
+    match expr {
+        Expr::Call { args, target } => {
+            let mut arg_results = vec![];
+            for arg in args {
+                arg_results.push(eval(arg)?);
+            }
 
-      let res = match target.as_str() {
-        "Pi" => std::f64::consts::PI,
-        "Sin" => arg_results[0].sin(), // how to index a vec or return Err in a not verbose way here?
-        "Cos" => arg_results[0].cos(),
-        "Tan" => arg_results[0].tan(),
-        "Sum" => sum(&arg_results),
-        _ => return Err(format!("unknown function: {}", target)),
-      };
+            let res = match target.as_str() {
+                "Pi" => std::f64::consts::PI,
+                "Sin" => arg_results[0].sin(), // how to index a vec or return Err in a not verbose way here?
+                "Cos" => arg_results[0].cos(),
+                "Tan" => arg_results[0].tan(),
+                "Sum" => sum(&arg_results),
+                _ => return Err(format!("unknown function: {}", target)),
+            };
 
-      return Ok(res);
+            Ok(res)
+        }
+
+        Expr::Num { val } => Ok(*val),
+
+        Expr::BinOp {
+            op,
+            a: a_node,
+            b: b_node,
+        } => {
+            let a = eval(a_node)?;
+            let b = eval(b_node)?;
+            let res = match op {
+                Operator::Plus => a + b,
+                Operator::Minus => a - b,
+                Operator::Multiply => a * b,
+                Operator::Divide => a / b,
+            };
+            Ok(res)
+        }
     }
-
-    Expr::Num { val } => Ok(*val),
-
-    Expr::BinOp {
-      op,
-      a: a_node,
-      b: b_node,
-    } => {
-      let a = eval(a_node)?;
-      let b = eval(b_node)?;
-      let res = match op {
-        Operator::Plus => a + b,
-        Operator::Minus => a - b,
-        Operator::Multiply => a * b,
-        Operator::Divide => a / b,
-      };
-      return Ok(res);
-    }
-  };
 }
 
-fn sum(args: &Vec<f64>) -> f64 {
-  let mut sum: f64 = 0.0;
-  for arg in args {
-    sum += arg;
-  }
-  return sum;
+fn sum(args: &[f64]) -> f64 {
+    let mut sum: f64 = 0.0;
+    for arg in args {
+        sum += arg;
+    }
+    sum
 }
--- src/lex.rs
+++ src/lex.rs
@@ -2,96 +2,97 @@ use regex;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TokenTy {
-  Whitespace,
-  Ident,
-  Number,
-  LParen,
-  RParen,
-  Comma,
-  Operator,
-  EOF,
+    Whitespace,
+    Ident,
+    Number,
+    LParen,
+    RParen,
+    Comma,
+    Operator,
+    EOF,
 }
 
 #[derive(Clone, Debug)]
 pub struct Token {
-  pub text: String,
-  pub ty: TokenTy,
+    pub text: String,
+    pub ty: TokenTy,
 }
 
 struct Matcher {
-  re: regex::Regex,
-  ty: TokenTy,
-  keep: bool,
+    re: regex::Regex,
+    ty: TokenTy,
+    keep: bool,
 }
 
 pub fn lex(input: &str) -> Result<Vec<Token>, String> {
-  let matchers = build_matchers();
+    let matchers = build_matchers();
 
-  let mut tokens = vec![];
-  let mut idx = 0;
-  while idx < input.len() {
-    let mut found_match = false;
-    for matcher in &matchers {
-      let match_res = matcher.re.find(&input[idx..]);
-      match match_res {
-        Some(x) => {
-          let tok = Token {
-            text: String::from(x.as_str()),
-            ty: matcher.ty,
-          };
-          idx += tok.text.len();
-          if matcher.keep {
-            tokens.push(tok);
-          }
-          found_match = true;
-          break;
+    let mut tokens = vec![];
+    let mut idx = 0;
+    while idx < input.len() {
+        let mut found_match = false;
+        for matcher in &matchers {
+            let match_res = matcher.re.find(&input[idx..]);
+            match match_res {
+                Some(x) => {
+                    let tok = Token {
+                        text: String::from(x.as_str()),
+                        ty: matcher.ty,
+                    };
+                    idx += tok.text.len();
+                    if matcher.keep {
+                        tokens.push(tok);
+                    }
+                    found_match = true;
+                    break;
+                }
+                _ => continue,
+            }
         }
-        _ => continue,
-      }
+        if !found_match {
+            return Err(format!("Could not parse token at idx: {}", idx));
+        };
     }
-    if !found_match {
-      return Err(format!("Could not parse token at idx: {}", idx));
-    };
-  }
-  return Ok(tokens);
+    Ok(tokens)
 }
 
+#[allow(clippy::trivial_regex)]
 fn build_matchers() -> Vec<Matcher> {
-  return vec![
-    Matcher {
-      re: regex::Regex::new(r"^[\s\n]+").unwrap(),
-      ty: TokenTy::Whitespace,
-      keep: false,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^[a-zA-Z][a-zA-Z0-9]*").unwrap(),
-      ty: TokenTy::Ident,
-      keep: true,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^[0-9]+(\.[0-9]+)?").unwrap(),
-      ty: TokenTy::Number,
-      keep: true,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^\(").unwrap(),
-      ty: TokenTy::LParen,
-      keep: true,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^\)").unwrap(),
-      ty: TokenTy::RParen,
-      keep: true,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^,").unwrap(),
-      ty: TokenTy::Comma,
-      keep: true,
-    },
-    Matcher {
-      re: regex::Regex::new(r"^[+\-\*/]").unwrap(),
-      ty: TokenTy::Operator,
-      keep: true,
-    },
-  ];
+    return vec![
+        Matcher {
+            re: regex::Regex::new(r"^[\s\n]+").unwrap(),
+            ty: TokenTy::Whitespace,
+            keep: false,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^[a-zA-Z][a-zA-Z0-9]*").unwrap(),
+            ty: TokenTy::Ident,
+            keep: true,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^[0-9]+(\.[0-9]+)?").unwrap(),
+            ty: TokenTy::Number,
+            keep: true,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^\(").unwrap(),
+            ty: TokenTy::LParen,
+            keep: true,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^\)").unwrap(),
+            ty: TokenTy::RParen,
+            keep: true,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^,").unwrap(),
+            ty: TokenTy::Comma,
+            keep: true,
+        },
+        Matcher {
+            re: regex::Regex::new(r"^[+\-\*/]").unwrap(),
+            ty: TokenTy::Operator,
+            keep: true,
+        },
+    ];
 }
